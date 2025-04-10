1e3036e3ff72f07c412bb563df197f34577363f2
AbhaySandha <abhaydec274@gmail.com>
2025-04-10 11:28:36 -0600

    19w image manipulator

--- 19 Image Manipulator/sketch.js
+++ 19 Image Manipulator/sketch.js
@@ -2,17 +2,18 @@
 // Sandha, Abhay
 // April 9, 2025
 
-let pilot;//p5.image    .width    .height
+let pilot; //p5.Image    .width  .height
 
 function preload() {
   pilot = loadImage("assets/aviator.png");
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(pilot.width, pilot.height);
 }
+
 function setPixelColor(pos, r, g, b) {
-  // assume pos points at a RED COMPONENT
+  //assume pos points at a RED component
   pixels[pos] = r;
   pixels[pos + 1] = g;
   pixels[pos + 2] = b;
@@ -20,63 +21,50 @@ function setPixelColor(pos, r, g, b) {
 
 function draw() {
   image(pilot, 0, 0);
-  loadPixels(); // fills pixels array
+  loadPixels();  //fills pixels array
   background(0);
 
-  let current = (mouseY * pilot.width + mouseX);
-  // fill(pixels[c],pixels[c+1],pixels[c+2]);
-  // circle(mouseX,mouseY, 50);
-
-  drawCharacters();
-
+  drawCharacter();
   //updatePixels();
 }
 
-function drawCharacters() {
-  // render an image using characters
+function drawCharacter(){
+  //render an image using characters
   fill(255);
 
-  for (let x = 0; x < width; x += 10) {
-    for (let y = 0; y < height; y += 10) {
-      let loc = (y * pilot.width + x) * 4;
-      let avg = avgPixel(loc);
-      if (avg > 200) {
-        text("&", x, y);
-      }
-      else if (avg > 150) {
-        text("*)", x, y);
-      }
-      else if (avg > 100) {
-        text("-", x, y);
-      }
-      else if (avg > 50) {
-        text(",", x, y);
-      }
+  for(let x = 0; x < width; x += 10){
+    for(let y = 0; y < height; y += 10){
+      let loc = (y*pilot.width + x) * 4;
+      let avg = avgPixel(loc);  // 0 - 255
+      if (avg > 200)       text("&",x,y);
+      else if (avg > 150)  text("*",x,y);
+      else if (avg > 100)  text("–",x,y);
+      else if (avg > 50)   text(",",x,y);
+      
     }
   }
 }
 
 function avgPixel(i) {
-  // i -> index of hte red component
+  //i → index of the red component
   let r = pixels[i];
   let g = pixels[i + 1];
   let b = pixels[i + 2];
-  return (r + g + b) / 3;
+  return (r+g+b)/3;
 }
 
-
 function greyscale() {
+  //a desaturation filter
   for (let i = 0; i < pixels.length; i += 4) {
     let avg = avgPixel(i);
-    setPixelColor(i, avg, avg, avg);
-
+    setPixelColor(i,avg, avg, avg);
   }
 }
 
 
 function boostImage() {
-  // a brightening filler; make each pixel brighter
-  let boost = map(mouseX, 0, width, - 100, 100);
+  //a brightnening filer; make each pixel brighter
+  let boost = map(mouseX, 0, width, -100, 100);
   for (let i = 0; i < pixels.length; i += 4) {
     let r = pixels[i] + boost;
     let g = pixels[i + 1] + boost;
@@ -84,6 +72,4 @@ function boostImage() {
     setPixelColor(i, r, g, b);
   }
 
-
 }
-
